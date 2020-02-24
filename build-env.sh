8ae33b6307fbac59511df5e485a45f7712b51130
Enda Kelly <ekel@endas-mbp.lan>
2020-02-24 23:10:12 +0000

    initial upload

---
+++ build-env.sh
@@ -0,0 +1,7 @@
+export S3_BUCKET_NAME=f-reco-auth-20022020-sam
+export STACK_NAME=emk-faceAuth
+export COLLECTION_NAME=emk-faceAuth-coll
+export USER_POOL_NAME=emk-faceAuth-usrPool
+export SIGN_UP_S3_BUCKET=f-reco-auth-20022020-signup
+export SIGN_IN_S3_BUCKET=f-reco-auth-20022020-signin
+export SES_FROM_ADDRESS=ekelly.aws01@gmail.com
